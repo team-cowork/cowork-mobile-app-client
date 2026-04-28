e73bb2b7fdf313bb1d14107d0dbe11aa8ea60ae4
LeeJueon <loolloo080313@gmail.com>
2026-04-28 19:29:49 +0900

    refactor(global): 디자인 시스템 패키지 분리

---
+++ packages/design_system/lib/design_system.dart
@@ -0,0 +1,9 @@
+library;
+
+export 'src/constants/app_spacing.dart';
+export 'src/theme/app_theme.dart';
+export 'src/theme/color/app_colors.dart';
+export 'src/theme/config/dark_theme.dart';
+export 'src/theme/config/light_theme.dart';
+export 'src/theme/icon/app_icon.dart';
+export 'src/theme/text_style/app_font.dart';
