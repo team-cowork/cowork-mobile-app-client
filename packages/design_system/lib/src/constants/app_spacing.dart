c830166ce513968d7651570f9665399e7c413f70
ryusuye0n <s25010@gsm.hs.kr>
2026-07-09 10:22:55 +0900

    refactor(shared): AppSpacing 10 추가

--- packages/design_system/lib/src/constants/app_spacing.dart
+++ packages/design_system/lib/src/constants/app_spacing.dart
@@ -7,6 +7,7 @@ class AppSpacing {
   static const double s4 = 4.0;
   static const double s6 = 6.0;
   static const double s8 = 8.0;
+  static const double s10 = 10.0;
   static const double s12 = 12.0;
   static const double s14 = 14.0;
   static const double s16 = 16.0;
