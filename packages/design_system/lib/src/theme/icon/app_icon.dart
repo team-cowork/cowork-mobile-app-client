2b32dd46349d29c94a3a47a926b64a3f37c16bd6
ryusuye0n <s25010@gsm.hs.kr>
2026-08-19 10:53:56 +0900

    feat(design-system): 알림 아이콘 추가

--- packages/design_system/lib/src/theme/icon/app_icon.dart
+++ packages/design_system/lib/src/theme/icon/app_icon.dart
@@ -87,4 +87,20 @@ class AppIcon {
     fit: BoxFit.contain,
   );
 
+  static Widget taskAssigned({double size = AppSize.iconSmall}) => SvgPicture.asset(
+    'assets/images/task_assigned.svg',
+    package: 'cowork_design_system',
+    width: size,
+    height: size,
+    fit: BoxFit.contain,
+  );
+
+  static Widget gitPush({double size = AppSize.iconSmall}) => SvgPicture.asset(
+    'assets/images/git_push.svg',
+    package: 'cowork_design_system',
+    width: size,
+    height: size,
+    fit: BoxFit.contain,
+  );
+
 }
